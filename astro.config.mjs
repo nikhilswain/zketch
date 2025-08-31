4be3c3f59af5a6a6ed729f230dded7133739cb49
nikhilswain <nikhilswain36@gmail.com>
2025-08-31 17:55:43 +0530

    favicon, adapter and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,27 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // Enable server-side rendering
-  integrations: [react()],
+  site: "https://zketch.pages.dev",
+
+  // Enable server-side rendering
+  output: "server",
+
+  integrations: [react(), sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: import.meta.env.PROD
+        ? { "react-dom/server": "react-dom/server.edge" }
+        : undefined,
+    },
   },
+
+  adapter: cloudflare(),
 });
